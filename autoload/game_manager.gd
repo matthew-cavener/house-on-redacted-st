d8320b0d1050b7e026f11dca15700c4207e2421a
Matthew Cavener <matthew.cavener@gmail.com>
2025-06-15 19:28:06 -0700

    feat: add evidence resource and room management

--- autoload/game_manager.gd
+++ autoload/game_manager.gd
@@ -1,1 +1,8 @@
 extends Node
+
+
+var current_room: String = "house_exterior"
+var room_history: Array[String] = []
+
+func change_room(new_room: String) -> void:
+    pass
---
+++ data/resources/evidence_resource.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name EvidenceResource
+
+@export var id: String
+@export var name: String
+@export var description: String
+@export var image: Texture2D
