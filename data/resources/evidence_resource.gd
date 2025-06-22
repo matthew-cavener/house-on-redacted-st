7b9273de533290395062ab893eba427e5781d3e6
Matthew Cavener <matthew.cavener@gmail.com>
2025-06-21 19:43:43 -0700

    feat: add support for evidence that exists in the background

--- data/resources/evidence_resource.gd
+++ data/resources/evidence_resource.gd
@@ -5,5 +5,18 @@ extends Resource
 @export var id: String
 @export var name: String
 @export var description: String
-@export var image: Texture2D
-@export var hero_image: Texture2D
+@export var image: Texture2D : set = set_evidence_image
+@export var hero_image: Texture2D : set = set_evidence_hero_image
+@export var shows_in_background: bool = true : set = set_evidence_shows_in_background
+
+func set_evidence_image(value: Texture2D):
+    image = value
+    emit_changed()
+
+func set_evidence_hero_image(value: Texture2D):
+    hero_image = value
+    emit_changed()
+
+func set_evidence_shows_in_background(value: bool):
+    shows_in_background = value
+    emit_changed()
--- scenes/rooms/house_exterior.gd
+++
@@ -1,2 +0,0 @@
-class_name HouseExterior
-extends Control
