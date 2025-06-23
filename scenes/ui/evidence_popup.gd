644fada4c66ab97afd4479eaea5292f741478c23
Matthew Cavener <matthew.cavener@gmail.com>
2025-06-22 23:17:13 -0700

    feat: unify evidence popup layout

--- scenes/ui/evidence_popup.gd
+++ scenes/ui/evidence_popup.gd
@@ -7,10 +7,6 @@ extends Control
 @onready var hero_image: TextureRect = $PopupContainer/HeroLayout/HeroImage
 @onready var hero_text_background: TextureRect = $PopupContainer/HeroLayout/TextBackgroundTexture
 @onready var hero_text: RichTextLabel = $PopupContainer/HeroLayout/TextBackgroundTexture/HeroText
-@onready var overlay_layout: Control = $PopupContainer/OverlayLayout
-@onready var overlay_image: TextureRect = $PopupContainer/OverlayLayout/OverlayImage
-@onready var description_texture: TextureRect = $PopupContainer/OverlayLayout/DescriptionTexture
-@onready var description_label: RichTextLabel = $PopupContainer/OverlayLayout/DescriptionLabel
 @onready var close_area: Control = $CloseArea
 
 var current_evidence: EvidenceResource
@@ -27,22 +23,22 @@ func show_evidence(evidence: EvidenceResource):
 func _setup_display():
 	if not current_evidence:
 		return
+	hero_layout.show()
+	hero_text.text = current_evidence.description
+	var notepad_width = 350
+	hero_text_background.custom_minimum_size.x = notepad_width
+	hero_text_background.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
 
 	if current_evidence.hero_image:
-		hero_layout.show()
-		overlay_layout.hide()
+		hero_image.show()
 		hero_image.texture = current_evidence.hero_image
-		hero_text.text = current_evidence.description
 		hero_image.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 		hero_image.custom_minimum_size.x = 400
-		hero_text.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-		hero_text.fit_content = true
+		hero_layout.alignment = BoxContainer.ALIGNMENT_BEGIN
 	else:
-		hero_layout.hide()
-		overlay_layout.show()
-		overlay_image.texture = current_evidence.image
-		overlay_image.modulate = Color(1, 1, 1, 0.4)
-		description_label.text = current_evidence.description
+		hero_image.hide()
+		hero_layout.alignment = BoxContainer.ALIGNMENT_CENTER
+	hero_text.fit_content = true
 
 func _on_close_area_input(event: InputEvent):
 	if event is InputEventMouseButton and event.pressed:
