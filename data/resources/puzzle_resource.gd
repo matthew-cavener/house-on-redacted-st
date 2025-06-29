123c0c8b678af421cb2a480870fa52ce5c88f3de
Matthew Cavener <matthew.cavener@gmail.com>
2025-06-29 11:56:13 -0700

    feat: add navigation puzzle evaluation

--- data/resources/puzzle_resource.gd
+++ data/resources/puzzle_resource.gd
@@ -10,13 +10,30 @@ extends Resource
 @export_multiline var failure_feedback: String = "Your paperwork has too many errors. Please try again."
 
 
-func validate_solution(user_selections: Dictionary) -> Dictionary:
+func validate_solution(user_selections: Dictionary, room_history: Array[String] = []) -> Dictionary:
 	var incorrect_count = 0
-	for field in dropdown_fields:
-		var user_answer = user_selections.get(field.field_name, "")
-		var is_valid = field.is_valid_answer(user_answer)
-		if not is_valid:
-			incorrect_count += 1
+	if puzzle_id == "answer_key_9" and not room_history.is_empty():
+		var navigation_history = room_history
+		var required_rooms = dropdown_fields.size()
+		var recent_rooms: Array[String] = []
+		if navigation_history.size() >= required_rooms:
+			recent_rooms = navigation_history.slice(-required_rooms)
+		else:
+			recent_rooms = navigation_history
+		for i in range(dropdown_fields.size()):
+			var field = dropdown_fields[i]
+			var room_answer = ""
+			if i < recent_rooms.size():
+				room_answer = recent_rooms[i]
+			var is_valid = field.is_valid_answer(room_answer)
+			if not is_valid:
+				incorrect_count += 1
+	else:
+		for field in dropdown_fields:
+			var user_answer = user_selections.get(field.field_name, "")
+			var is_valid = field.is_valid_answer(user_answer)
+			if not is_valid:
+				incorrect_count += 1
 	var is_correct = incorrect_count == 0
 	var is_close = incorrect_count > 0 and incorrect_count <= incorrect_fields_for_close_feedback
 	var feedback = ""
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,1 +1,40 @@
 extends Node
+
+@onready var debug_solve_puzzle_button = $GUI/DebugSolvePuzzle
+
+func _ready():
+	if debug_solve_puzzle_button:
+		debug_solve_puzzle_button.visible = OS.is_debug_build()
+		debug_solve_puzzle_button.pressed.connect(_on_debug_solve_puzzle_pressed)
+
+func _on_debug_solve_puzzle_pressed():
+	print("Debug: Attempting to solve current puzzle...")
+	var current_puzzle = GameManager.current_puzzle
+	if not current_puzzle:
+		print("Debug: No active puzzle to solve")
+		return
+	print("Debug: Solving puzzle '%s' (%s)" % [current_puzzle.puzzle_name, current_puzzle.puzzle_id])
+	if current_puzzle.puzzle_id == "answer_key_9":
+		_solve_navigation_puzzle(current_puzzle)
+	else:
+		_solve_dropdown_puzzle(current_puzzle)
+
+func _solve_navigation_puzzle(puzzle: PuzzleResource):
+	print("Debug: Solving navigation puzzle by navigating to required rooms...")
+	var required_rooms: Array[String] = []
+	for field in puzzle.dropdown_fields:
+		required_rooms.append(field.answer)
+	print("Debug: Required room sequence: %s" % str(required_rooms))
+	for room in required_rooms:
+		print("Debug: Navigating to room: %s" % room)
+		GameManager.change_room(room)
+		await get_tree().create_timer(0.5).timeout
+
+func _solve_dropdown_puzzle(puzzle: PuzzleResource):
+	print("Debug: Solving dropdown puzzle by filling correct answers...")
+	for field in puzzle.dropdown_fields:
+		var correct_answer = field.answer
+		print("Debug: Setting field '%s' to '%s'" % [field.field_name, correct_answer])
+		EventBus.dropdown_selection_changed.emit(puzzle.puzzle_id, field.field_name, correct_answer)
+		await get_tree().create_timer(0.2).timeout
+	print("Debug: All dropdown fields filled with correct answers")
