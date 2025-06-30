de7f01d3de0dc0460b5eb5fbe224ffdb5603abb0
Matthew Cavener <matthew.cavener@gmail.com>
2025-06-29 17:11:08 -0700

    feat: add support for room unlock sounds

--- autoload/event_bus.gd
+++ autoload/event_bus.gd
@@ -7,6 +7,7 @@ signal evidence_popup_closed()
 signal puzzle_blank_filled_in(blank_id: String)
 signal puzzle_failed()
 signal puzzle_solved(puzzle_id: String)
+signal rooms_unlocked()
 
 signal dropdown_selection_changed(puzzle_id: String, field_name: String, selected_value: String)
 signal puzzle_evaluated(puzzle_id: String, result: Dictionary)
